8a7641d94eba4327f93ee30b5970e04d7581183b
Zander, Julian <Julian.Zander@adesso.de>
2022-11-15 14:05:28 +0100

    Add the articles view

--- carah_app/lib/ui/Articles/articles_overview.dart
+++ carah_app/lib/ui/Articles/articles_overview.dart
@@ -1,3 +1,4 @@
+import 'package:carah_app/ui/Articles/articles_content.dart';
 import 'package:carah_app/ui/Articles/articles_items.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -9,13 +10,10 @@ class ArticlesOverview extends StatefulWidget {
   _ArticlesOverview createState() => _ArticlesOverview();
 }
 
-
 class _ArticlesOverview extends State<ArticlesOverview> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // NEW from here ...
       appBar: AppBar(
         title: const Text('Articles Overview'),
         actions: [
@@ -28,26 +26,32 @@ class _ArticlesOverview extends State<ArticlesOverview> {
       ),
       body: ListView.builder(
         // to here.
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(0.0),
         itemCount: articlesItemsList.length,
         itemBuilder: (context, i) {
           return ListTile(
             title: Text(
               articlesItemsList[i].title.toString(),
             ),
-            trailing: Wrap(spacing: 12, children: <Widget>[
+            trailing:
               IconButton(
                 icon: Icon(articlesItemsList[i].saved
                     ? Icons.favorite
                     : Icons.favorite_border),
                 onPressed: () => {
                   setState(() {
-                    articlesItemsList[i].saved =
-                    !articlesItemsList[i].saved;
-                })
+                    articlesItemsList[i].saved = !articlesItemsList[i].saved;
+                  })
                 },
               ),
-            ]),
+
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const ArticlesContent()),
+              );
+            },
           );
         },
       ),
