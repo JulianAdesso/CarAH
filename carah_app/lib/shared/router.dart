3392856132034c5c59a5abf42228a8c35a6ba807
Zander, Julian <Julian.Zander@adesso.de>
2022-12-27 15:21:49 +0100

    Add image Gallery

--- carah_app/lib/shared/router.dart
+++ carah_app/lib/shared/router.dart
@@ -1,5 +1,6 @@
 import 'package:carah_app/ui/Articles/articles_categories.dart';
 import 'package:carah_app/ui/Articles/articles_content.dart';
+import 'package:carah_app/ui/Articles/articles_gallery.dart';
 import 'package:carah_app/ui/Articles/articles_overview.dart';
 import 'package:carah_app/ui/home/home_page.dart';
 import 'package:go_router/go_router.dart';
@@ -13,7 +14,9 @@ final router = GoRouter(
     GoRoute(path: '/articles/:id',
     builder: (context, state) => ArticlesOverview(id: state.params['id']!)),
     GoRoute(path: '/article/:id',
-    builder: (context, state) => ArticlesContent(id: state.params['id']!))
+    builder: (context, state) => ArticlesContent(id: state.params['id']!)),
+    GoRoute(path: '/article/:id/gallery',
+        builder: (context, state) => ArticlesGallery(id: state.params['id']!))
 
   ],
 );
