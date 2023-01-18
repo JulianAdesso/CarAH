35429a9613dfdab6afe44a7e272bbbdb5f9f8f18
malte.sichtermann <malte.sichtermann@adesso.de>
2023-01-18 10:08:21 +0200

    Fixed Search function

--- carah_app/lib/providers/content_provider.dart
+++ carah_app/lib/providers/content_provider.dart
@@ -17,13 +17,6 @@ class ContentProvider<P extends Content> extends ChangeNotifier {
 
   final _offlineBox = Hive.box('myBox');
   final _baseURL = 'http://h2992008.stratoserver.net:8080/api/v2/CarAH';
-  List<String> _allArticlesByUuid = [];
-  List<String> _allQuestionsByUuid = [];
-
-  List<Content> _AllContent = [];
-
-  final _offlineBox = Hive.box('myBox');
-  final _baseURL = 'http://h2992008.stratoserver.net:8080/api/v2/CarAH';
 
   get items => _items;
   get allArticlesUuid => _allArticlesByUuid;
@@ -55,41 +48,12 @@ class ContentProvider<P extends Content> extends ChangeNotifier {
       items.removeWhere((element) => element.title == ""); //The "Article Images" Folder has been loaded without title
       _offlineBox.put("articles", items);
     } else {
-      if(_offlineBox.get("questions") != null ||  _offlineBox.get("questions") != null) {
-        var tmp1 = _offlineBox.get("questions").cast<Content>();
-        var tmp2 = _offlineBox.get("articles").cast<Content>();
-        _items.addAll(tmp1 as List<P>);
-        _items.addAll(tmp2 as List<P>);
-      var tmpArticlesUuid = _offlineBox.get("articles").cast<Content>();
-      _allArticlesByUuid =   (tmpArticlesUuid as List<Content>).map((content) => (content.uuid.toString())).toList();
-      var tmpQuestionUuid = _offlineBox.get("questions").cast<Content>();
-      _allQuestionsByUuid =   (tmpQuestionUuid as List<Content>).map((content) => (content.uuid.toString())).toList();
-      }
-    }
-    notifyListeners();
-}
-  fetchAllContent () async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
-    if (connectivityResult == ConnectivityResult.mobile ||
-        connectivityResult == ConnectivityResult.wifi) {
-      var articlesFromCMS = await http.get(
-          Uri.parse(
-              '$_baseURL/nodes'),
-          headers: {
-            "Content-Type": "application/json",
-          });
-      items =
-          jsonDecode(utf8.decoder.convert(articlesFromCMS.bodyBytes))['data']
-              .map<Article>((element) {
-            return Article.fromJson(element);
-          }).toList();
-      items.removeWhere((element) => element.title == ""); //The "Article Images" Folder has been loaded without title
-      _offlineBox.put("articles", items);
-    } else {
-      //_items.clear();
-      _items = _offlineBox.get("questions").cast<Content>();
-      _items.addAll(_offlineBox.get("articles").cast<Content>());
-      print(items.length);
+      var tmp1 = _offlineBox.get("questions").cast<Content>();
+      var tmp2 = _offlineBox.get("articles").cast<Content>();
+
+      _items.addAll(tmp1 as List<P>);
+      _items.addAll(tmp2 as List<P>);
+
       var tmpArticlesUuid = _offlineBox.get("articles").cast<Content>();
       _allArticlesByUuid =   (tmpArticlesUuid as List<Content>).map((content) => (content.uuid.toString())).toList();
       var tmpQuestionUuid = _offlineBox.get("questions").cast<Content>();
