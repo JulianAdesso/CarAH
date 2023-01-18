fc5c5578e497f83d4467d7dc1c5e54efe69973f3
Gundlach, Veronika <Veronika.Gundlach@adesso.de>
2023-01-18 09:15:16 +0100

    disable buttons in home when in offline mode

--- carah_app/lib/ui/home/home_page.dart
+++ carah_app/lib/ui/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:carah_app/ui/home/navigation_items.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -6,6 +7,29 @@ import '../bottom_navbar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  Future<bool> checkConnectivity() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool isEnabled(ListItem element, AsyncSnapshot<bool> snapshot){
+    if(element.routerLink == null){
+      return false;
+    } else if(snapshot.hasData && snapshot.data!){
+      return true;
+    } else if(snapshot.hasData && !snapshot.data! && element.availableInOfflineMode){
+      return true;
+    } else {
+      return false;
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,49 +41,72 @@ class HomePage extends StatelessWidget {
         body: ListView(
           padding: const EdgeInsets.all(15),
           children: homeItemsList.map((element) {
-            return GestureDetector(
-              onTap: () => element.routerLink != null ? context.push(element.routerLink!) : null,
-              child: Card(
-                  color: Theme.of(context).colorScheme.tertiaryContainer,
-                  margin: const EdgeInsets.symmetric(vertical: 10.0),
-                  child: Row(
-                    children: [Flexible(
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 5),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                flex: 1,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(bottom: 15.0),
-                                      child: Text(element.title,
-                                          style:
-                                              Theme.of(context).textTheme.titleLarge,
+            return FutureBuilder<bool>(
+                future: checkConnectivity(),
+                builder: (context, snapshot) {
+                  return GestureDetector(
+                    onTap: isEnabled(element, snapshot)
+                        ? () => context.push(element.routerLink!)
+                        : null,
+                    child: Card(
+                        color: isEnabled(element, snapshot) ? Theme.of(context).colorScheme.tertiaryContainer : Theme.of(context).disabledColor,
+                        margin: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: Row(
+                          children: [
+                            Flexible(
+                              child: Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 5),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 12.0, horizontal: 16.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Expanded(
+                                        flex: 1,
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  bottom: 15.0),
+                                              child: Text(
+                                                element.title,
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .titleLarge,
+                                              ),
+                                            ),
+                                            Text(
+                                              element.description!,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyMedium,
+                                              overflow: TextOverflow.visible,
+                                            )
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                    Text(element.description!,
-                                    style: Theme.of(context).textTheme.bodyMedium,
-                                    overflow: TextOverflow.visible,)
-                                  ],
+                                      Icon(
+                                        element.icon,
+                                        size: 50.0,
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
-                              Icon(element.icon,
-                                size: 50.0,),
-                            ],
-                          ),
-                        ),
-                      ),
-                    )],
-                  )),
-            );
+                            )
+                          ],
+                        )),
+                  );
+                });
           }).toList(),
         ),
         bottomNavigationBar: BottomNavbar(currIndex: 0));
--- carah_app/lib/ui/home/navigation_items.dart
+++ carah_app/lib/ui/home/navigation_items.dart
@@ -7,23 +7,24 @@ class ListItem {
   String? description;
   IconData icon;
   String? routerLink;
+  bool availableInOfflineMode;
 
-  ListItem({this.uuid, required this.title, this.description, required this.icon, this.routerLink});
+  ListItem({this.uuid, required this.title, this.description, required this.icon, this.routerLink, required this.availableInOfflineMode});
 
 
 }
 
 final List<ListItem> homeItemsList  = [
-  ListItem(title: 'Tokoloho Articles', description: 'Lipsum', icon: Icons.article_outlined, routerLink: '/articles_categories'),
-  ListItem(title: 'FAQ', description: 'Frequently Asked Questions', icon: Icons.forum_outlined, routerLink: '/faq_categories'),
-  ListItem(title: 'Tokoloho Guides', description: 'Basic Information about HIV/AIDS & TB', icon: Icons.directions_outlined, routerLink: '/guides_categories'),
-  ListItem(title: 'Events', description: 'Lipsum', icon: Icons.event_outlined, routerLink: '/events'),
-  ListItem(title: 'Medicine pick up dates', description: 'Lipsum', icon: Icons.medication_outlined, routerLink: '/pick_up_dates')
+  ListItem(title: 'Tokoloho Articles', description: 'Lipsum', icon: Icons.article_outlined, routerLink: '/articles_categories', availableInOfflineMode: true),
+  ListItem(title: 'FAQ', description: 'Frequently Asked Questions', icon: Icons.forum_outlined, routerLink: '/faq_categories', availableInOfflineMode: false),
+  ListItem(title: 'Tokoloho Guides', description: 'Basic Information about HIV/AIDS & TB', icon: Icons.directions_outlined, routerLink: '/guides_categories', availableInOfflineMode: false),
+  ListItem(title: 'Events', description: 'Lipsum', icon: Icons.event_outlined, routerLink: '/events', availableInOfflineMode: false),
+  ListItem(title: 'Medicine pick up dates', description: 'Lipsum', icon: Icons.medication_outlined, routerLink: '/pick_up_dates', availableInOfflineMode: false)
 ];
 
 final List<ListItem> bottomNavbarItems = [
-  ListItem(title: 'Home', icon: Icons.home_outlined, routerLink: '/'),
-  ListItem(title: 'Search', icon: Icons.search, routerLink: '/search'),
-  ListItem(title: 'Favorites', icon: Icons.favorite_border_outlined, routerLink: '/favorites'),
-  ListItem(title: 'Settings', icon: Icons.settings_outlined, routerLink: '/settings')
+  ListItem(title: 'Home', icon: Icons.home_outlined, routerLink: '/', availableInOfflineMode: true),
+  ListItem(title: 'Search', icon: Icons.search, routerLink: '/search', availableInOfflineMode: true),
+  ListItem(title: 'Favorites', icon: Icons.favorite_border_outlined, routerLink: '/favorites', availableInOfflineMode: true),
+  ListItem(title: 'Settings', icon: Icons.settings_outlined, routerLink: '/settings', availableInOfflineMode: true)
 ];
