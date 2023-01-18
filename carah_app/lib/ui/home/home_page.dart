514c5fa7091c29329ce567be264265ed716d6e95
yauheniya.tushynskaya <yauheniya.tushynskaya@adesso.de>
2023-01-18 08:34:46 +0200

    Change color of home card items

--- carah_app/lib/ui/home/home_page.dart
+++ carah_app/lib/ui/home/home_page.dart
@@ -41,6 +41,49 @@ class HomePage extends StatelessWidget {
         body: ListView(
           padding: const EdgeInsets.all(15),
           children: homeItemsList.map((element) {
+            return GestureDetector(
+              onTap: () => element.routerLink != null ? context.push(element.routerLink!) : null,
+              child: Card(
+                  color: Theme.of(context).colorScheme.background,
+                  margin: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Row(
+                    children: [Flexible(
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(bottom: 15.0),
+                                      child: Text(element.title,
+                                          style:
+                                              Theme.of(context).textTheme.titleLarge,
+                                      ),
+                                    ),
+                                    Text(element.description!,
+                                    style: Theme.of(context).textTheme.bodyMedium,
+                                    overflow: TextOverflow.visible,)
+                                  ],
+                                ),
+                              ),
+                              Icon(element.icon,
+                                size: 50.0,),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )],
+                  )),
+            );
             return FutureBuilder<bool>(
                 future: checkConnectivity(),
                 builder: (context, snapshot) {
