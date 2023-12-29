1712fc872ca8347f66a9d8fade9e3b7389de0f32
gundlach <Veronika.Gundlach@adesso.de>
2023-12-29 11:32:59 +0100

    also show privacy policy on web..

--- carah_app/lib/ui/settings/privacy_policy_widget.dart
+++ carah_app/lib/ui/settings/privacy_policy_widget.dart
@@ -1,7 +1,8 @@
 import 'package:carah_app/shared/loading_spinner_widget.dart';
-import 'package:easy_pdf_viewer/easy_pdf_viewer.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router_flow/go_router_flow.dart';
+import 'package:pdfx/pdfx.dart';
 
 import '../../model/bottom_navbar_index.dart';
 import '../../shared/bottom_navbar.dart';
@@ -9,8 +10,10 @@ import '../../shared/bottom_navbar.dart';
 class PrivacyPolicyWidget extends StatelessWidget {
   const PrivacyPolicyWidget({super.key});
 
-  Future<PDFDocument> getAsset() async {
-    PDFDocument doc = await PDFDocument.fromAsset('assets/privacy_policy.pdf');
+  PdfController getAsset() {
+    PdfController doc = PdfController(
+      document: PdfDocument.openAsset('assets/privacy_policy.pdf'),
+    );
     return doc;
   }
 
@@ -24,19 +27,18 @@ class PrivacyPolicyWidget extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.primary,
         title: const Text('Privacy Policy'),
       ),
-      body: FutureBuilder(future: getAsset(),
-      builder: (context, data) {
-        if(data.hasData) {
-          return PDFViewer(document: data.data!,
-          scrollDirection: Axis.vertical,
-          showPicker: false,
-          showNavigation: false,);
-        }
-        else {
-          return const LoadingSpinnerWidget();
-        }
-      }),
-      bottomNavigationBar: BottomNavbar(currIndex: BottomNavbarIndex.home.index),
+      body: PdfView(
+        controller: getAsset(),
+        scrollDirection: Axis.vertical,
+        pageSnapping: kIsWeb ? false : true,
+        builders: PdfViewBuilders<DefaultBuilderOptions>(
+            options: const DefaultBuilderOptions(),
+            documentLoaderBuilder: (context) {
+              return const LoadingSpinnerWidget();
+            }),
+      ),
+      bottomNavigationBar:
+          BottomNavbar(currIndex: BottomNavbarIndex.home.index),
     );
   }
 }
